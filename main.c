a3a34779adb61a40bbc2fb929a2a7061aea7d120
DeStep <tankt34.stepanoff@yandex.ru>
2023-06-11 18:16:08 +0300

    Без русификатора

--- main.c
+++ main.c
@@ -1,217 +1,311 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdbool.h>
+#include <locale.h>
 
-#define MAX_TREE_HT 256
+// Структура для представления узла в дереве Хаффмана
+struct HuffmanNode {
+    unsigned char data; // Данные символа
+    unsigned int frequency; // Частота символа
+    struct HuffmanNode* left; // Левый потомок
+    struct HuffmanNode* right; // Правый потомок
+};
 
-typedef struct MinHeapNode {
-    char data;
-    unsigned freq;
-    struct MinHeapNode* left, * right;
-} MinHeapNode;
+// Структура для представления очереди приоритетов
+struct PriorityQueue {
+    unsigned int size; // Размер очереди
+    unsigned int capacity; // Емкость очереди
+    struct HuffmanNode** nodes; // Массив узлов
+};
 
-typedef struct MinHeap {
-    unsigned size;
-    unsigned capacity;
-    MinHeapNode** array;
-} MinHeap;
-
-typedef struct HuffmanNode {
-    char data;
-    unsigned int freq;
-    struct HuffmanNode* left, * right;
-} HuffmanNode;
-
-typedef struct HuffmanTree {
-    HuffmanNode* root;
-} HuffmanTree;
-
-MinHeapNode* newMinHeapNode(char data, unsigned freq) {
-    MinHeapNode* node = (struct MinHeapNode*)malloc(sizeof(struct MinHeapNode));
-    node->left = node->right = NULL;
+// Создание нового узла дерева Хаффмана
+struct HuffmanNode* createNode(unsigned char data, unsigned int frequency) {
+    struct HuffmanNode* node = (struct HuffmanNode*)malloc(sizeof(struct HuffmanNode));
     node->data = data;
-    node->freq = freq;
+    node->frequency = frequency;
+    node->left = NULL;
+    node->right = NULL;
     return node;
 }
 
-MinHeap* createMinHeap(unsigned capacity) {
-    MinHeap* minHeap = (MinHeap*)malloc(sizeof(MinHeap));
-    minHeap->size = 0;
-    minHeap->capacity = capacity;
-    minHeap->array = (MinHeapNode**)malloc(minHeap->capacity * sizeof(MinHeapNode*));
-    return minHeap;
+// Создание очереди приоритетов
+struct PriorityQueue* createPriorityQueue(unsigned int capacity) {
+    struct PriorityQueue* queue = (struct PriorityQueue*)malloc(sizeof(struct PriorityQueue));
+    queue->size = 0;
+    queue->capacity = capacity;
+    queue->nodes = (struct HuffmanNode**)malloc(capacity * sizeof(struct HuffmanNode*));
+    return queue;
 }
 
-void swapMinHeapNode(MinHeapNode **a, MinHeapNode **b) {
-    MinHeapNode* t = *a;
+// Проверка, является ли очередь приоритетов пустой
+bool isQueueEmpty(struct PriorityQueue* queue) {
+    return queue->size == 0;
+}
+
+// Обмен двух узлов в очереди приоритетов
+void swapNodes(struct HuffmanNode** a, struct HuffmanNode** b) {
+    struct HuffmanNode* temp = *a;
     *a = *b;
-    *b = t;
+    *b = temp;
 }
 
-void minHeapify(MinHeap* minHeap, int idx) {
-    int smallest = idx;
-    int left = 2 * idx + 1;
-    int right = 2 * idx + 2;
-    if (left < minHeap->size && minHeap->array[left]->freq < minHeap->array[smallest]->freq)
+// Просеивание вниз в очереди приоритетов
+void heapify(struct PriorityQueue* queue, int index) {
+    int smallest = index;
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+
+    if (left < queue->size && queue->nodes[left]->frequency < queue->nodes[smallest]->frequency)
         smallest = left;
-    if (right < minHeap->size && minHeap->array[right]->freq < minHeap->array[smallest]->freq)
+
+    if (right < queue->size && queue->nodes[right]->frequency < queue->nodes[smallest]->frequency)
         smallest = right;
-    if (smallest != idx) {
-        swapMinHeapNode(&minHeap->array[smallest], &minHeap->array[idx]);
-        minHeapify(minHeap, smallest);
+
+    if (smallest != index) {
+        swapNodes(&queue->nodes[index], &queue->nodes[smallest]);
+        heapify(queue, smallest);
     }
 }
 
-MinHeapNode* extractMin(MinHeap* minHeap) {
-    MinHeapNode* node = minHeap->array[0];
-    minHeap->array[0] = minHeap->array[minHeap->size - 1];
-    --minHeap->size;
-    minHeapify(minHeap, 0);
-    return node;
-}
+// Добавление узла в очередь приоритетов
+void enqueue(struct PriorityQueue* queue, struct HuffmanNode* node) {
+    if (queue->size == queue->capacity)
+        return;
 
-void insertMinHeap(MinHeap* minHeap, MinHeapNode* minHeapNode) {
-    ++minHeap->size;
-    int i = minHeap->size - 1;
-    while (i && minHeapNode->freq < minHeap->array[(i - 1) / 2]->freq) {
-        minHeap->array[i] = minHeap->array[(i - 1) / 2];
+    int i = queue->size;
+    queue->nodes[i] = node;
+    queue->size++;
+
+    while (i != 0 && queue->nodes[(i - 1) / 2]->frequency > queue->nodes[i]->frequency) {
+        swapNodes(&queue->nodes[(i - 1) / 2], &queue->nodes[i]);
         i = (i - 1) / 2;
     }
-    minHeap->array[i] = minHeapNode;
 }
 
-MinHeap* createAndBuildMinHeap(char data[], int freq[], int size) {
-    MinHeap* minHeap = createMinHeap(size);
+// Удаление и возврат узла с наименьшей частотой из очереди приоритетов
+struct HuffmanNode* dequeue(struct PriorityQueue* queue) {
+    if (isQueueEmpty(queue))
+        return NULL;
+
+    struct HuffmanNode* node = queue->nodes[0];
+    queue->nodes[0] = queue->nodes[queue->size - 1];
+    queue->size--;
+
+    heapify(queue, 0);
+
+    return node;
+}
+
+// Построение дерева Хаффмана на основе частот символов
+struct HuffmanNode* buildHuffmanTree(unsigned char data[], unsigned int frequency[], unsigned int size) {
+    struct HuffmanNode *left, *right, *top;
+
+    // Создание очереди приоритетов и заполнение ее узлами-листьями
+    struct PriorityQueue* queue = createPriorityQueue(size);
     for (int i = 0; i < size; ++i)
-        minHeap->array[i] = newMinHeapNode(data[i], freq[i]);
-    minHeap->size = size;
-    for (int i = (size - 1) / 2; i >= 0; i--)
-        minHeapify(minHeap, i);
-    return minHeap;
-}
+        enqueue(queue, createNode(data[i], frequency[i]));
 
-HuffmanNode* buildHuffmanTree(char data[], int freq[], int size) {
-    MinHeapNode* left, * right, * top;
-    MinHeap* minHeap = createAndBuildMinHeap(data, freq, size);
-    while (minHeap->size != 1) {
-        left = extractMin(minHeap);
-        right = extractMin(minHeap);
-        //top = (HuffmanNode*)malloc(sizeof(HuffmanNode));
-        top = newMinHeapNode('$', left->freq + right->freq);
+    // Построение дерева Хаффмана
+    while (!isQueueEmpty(queue)) {
+        left = dequeue(queue);
+        right = dequeue(queue);
+
+        top = createNode('$', left->frequency + right->frequency);
         top->left = left;
         top->right = right;
-        insertMinHeap(minHeap, (MinHeapNode*)top);
+
+        enqueue(queue, top);
     }
-    return (HuffmanNode*)extractMin(minHeap);
+
+    // Извлечение корня дерева Хаффмана
+    return dequeue(queue);
 }
 
-void printCodes(HuffmanNode* root, int arr[], int top, FILE* fp) {
-    if (root->left) {
-        arr[top] = 0;
-        printCodes(root->left, arr, top + 1, fp);
-    }
-    if (root->right) {
-        arr[top] = 1;
-        printCodes(root->right, arr, top + 1, fp);
-    }
-    if (!root->left && !root->right) {
-        fprintf(fp, "%c:", root->data);
-        for (int i = 0; i < top; ++i)
-            fprintf(fp, "%d", arr[i]);
-        fprintf(fp, "\n");
+// Запись бита в байт
+void writeBit(FILE* file, unsigned char* currentByte, unsigned char* bitPosition, bool bit) {
+    *currentByte <<= 1;
+    if (bit)
+        *currentByte |= 1;
+
+    (*bitPosition)++;
+    if (*bitPosition == 8) {
+        fwrite(currentByte, sizeof(unsigned char), 1, file);
+        *currentByte = 0;
+        *bitPosition = 0;
     }
 }
 
-void HuffmanCodes(char data[], int freq[], int size, FILE* fp) {
-    HuffmanNode* root = buildHuffmanTree(data, freq, size);
-    int arr[MAX_TREE_HT], top = 0;
-    printCodes(root, arr, top, fp);
+// Чтение бита из байта
+bool readBit(FILE* file, unsigned char* currentByte, unsigned char* bitPosition) {
+    if (*bitPosition == 0) {
+        fread(currentByte, sizeof(unsigned char), 1, file);
+        *bitPosition = 8;
+    }
+
+    bool bit = (*currentByte >> (*bitPosition - 1)) & 1;
+    (*bitPosition)--;
+    return bit;
 }
 
-void encode(FILE* input_fp, FILE* output_fp, FILE* code_fp) {
-    char data[MAX_TREE_HT];
-    int freq[MAX_TREE_HT] = { 0 };
-    char c;
-    while ((c = fgetc(input_fp)) != EOF) {
-        ++freq[c];
+// Запись заголовка с информацией о дереве Хаффмана в сжатый файл
+void writeHeader(FILE* compressedFile, struct HuffmanNode* root, unsigned int* dataSize) {
+    if (root == NULL)
+        return;
+
+    if (root->left == NULL && root->right == NULL) {
+        // Листовой узел
+        writeBit(compressedFile, NULL, NULL, true);
+        fwrite(&root->data, sizeof(unsigned char), 1, compressedFile);
+        (*dataSize)++;
+    } else {
+        // Внутренний узел
+        writeBit(compressedFile, NULL, NULL, false);
+        (*dataSize)++;
+        writeHeader(compressedFile, root->left, dataSize);
+        writeHeader(compressedFile, root->right, dataSize);
     }
-    int size = 0;
-    for (int i = 0; i < 256; ++i) {
-        if (freq[i]) {
-            data[size++] = i;
+}
+
+// Чтение заголовка из сжатого файла и построение дерева Хаффмана
+struct HuffmanNode* readHeader(FILE* compressedFile, unsigned char* currentByte, unsigned char* bitPosition) {
+    bool isLeaf = readBit(compressedFile, currentByte, bitPosition);
+
+    if (isLeaf) {
+        unsigned char data;
+        fread(&data, sizeof(unsigned char), 1, compressedFile);
+        return createNode(data, 0);
+    } else {
+        struct HuffmanNode* left = readHeader(compressedFile, currentByte, bitPosition);
+        struct HuffmanNode* right = readHeader(compressedFile, currentByte, bitPosition);
+        struct HuffmanNode* node = createNode('$', 0);
+        node->left = left;
+        node->right = right;
+        return node;
+    }
+}
+
+// Сжатие файла с использованием алгоритма Хаффмана
+void compressFile(const char* sourceFilePath, const char* compressedFilePath) {
+    FILE* sourceFile = fopen(sourceFilePath, "rb");
+    if (sourceFile == NULL) {
+        printf("Не удалось открыть исходный файл.\n");
+        return;
+    }
+
+    // Подсчет частоты символов в исходном файле
+    unsigned int frequency[256] = {0};
+    unsigned char data;
+    while (fread(&data, sizeof(unsigned char), 1, sourceFile))
+        frequency[data]++;
+
+    fclose(sourceFile);
+
+    // Построение дерева Хаффмана
+    struct HuffmanNode* root = buildHuffmanTree((unsigned char[]){0}, frequency, 256);
+
+    // Открытие сжатого файла для записи
+    FILE* compressedFile = fopen(compressedFilePath, "wb");
+    if (compressedFile == NULL) {
+        printf("Не удалось открыть сжатый файл.\n");
+        return;
+    }
+
+    // Запись заголовка в сжатый файл
+    unsigned int dataSize = 0;
+    writeHeader(compressedFile, root, &dataSize);
+
+    // Открытие исходного файла для чтения
+    sourceFile = fopen(sourceFilePath, "rb");
+    if (sourceFile == NULL) {
+        printf("Не удалось открыть исходный файл.\n");
+        return;
+    }
+
+    // Кодирование данных и запись в сжатый файл
+    unsigned char currentByte = 0;
+    unsigned char bitPosition = 0;
+    while (fread(&data, sizeof(unsigned char), 1, sourceFile)) {
+        struct HuffmanNode* current = root;
+        while (current->left != NULL && current->right != NULL) {
+            bool bit = (data >> bitPosition) & 1;
+            if (bit)
+                current = current->right;
+            else
+                current = current->left;
+
+            bitPosition++;
+            if (bitPosition == 8) {
+                bitPosition = 0;
+                fread(&data, sizeof(unsigned char), 1, sourceFile);
+            }
         }
+        writeBit(compressedFile, &currentByte, &bitPosition, (current->data >> 7) & 1);
     }
-    HuffmanCodes(data, freq, size, code_fp);
-    fseek(input_fp, 0, SEEK_SET);
-    unsigned char buffer = 0;
-    int bit_count = 0;
-    while ((c = fgetc(input_fp)) != EOF) {
-        char code[MAX_TREE_HT];
-        fseek(code_fp, 0, SEEK_SET);
-        while (fgets(code, MAX_TREE_HT, code_fp) != NULL) {
-            char symbol;
-            int len = strlen(code) - 2;
-            sscanf(code, "%c:", &symbol);
-            if (symbol == c) {
-                for (int i = 0; i < len; ++i) {
-                    if (code[i + 2] == '1') {
-                        buffer |= (1 << (7 - bit_count));
-                    }
-                    ++bit_count;
-                    if (bit_count == 8) {
-                        fputc(buffer, output_fp);
-                        buffer = 0;
-                        bit_count = 0;
-                    }
-                }
-                break;
+
+    // Запись последнего неполного байта, если есть
+    while (bitPosition != 0)
+        writeBit(compressedFile, &currentByte, &bitPosition, 0);
+
+    // Освобождение памяти, закрытие файлов
+    fclose(sourceFile);
+    fclose(compressedFile);
+    free(root);
+}
+
+// Распаковка сжатого файла
+void decompressFile(const char* compressedFilePath, const char* decompressedFilePath) {
+    FILE* compressedFile = fopen(compressedFilePath, "rb");
+    if (compressedFile == NULL) {
+        printf("Не удалось открыть сжатый файл.\n");
+        return;
+    }
+
+    // Чтение заголовка из сжатого файла и построение дерева Хаффмана
+    unsigned char currentByte = 0;
+    unsigned char bitPosition = 0;
+    struct HuffmanNode* root = readHeader(compressedFile, &currentByte, &bitPosition);
+
+    // Открытие распакованного файла для записи
+    FILE* decompressedFile = fopen(decompressedFilePath, "wb");
+    if (decompressedFile == NULL) {
+        printf("Не удалось открыть распакованный файл.\n");
+        return;
+    }
+
+    // Раскодирование данных и запись в распакованный файл
+    struct HuffmanNode* current = root;
+    while (fread(&currentByte, sizeof(unsigned char), 1, compressedFile)) {
+        for (int i = 7; i >= 0; --i) {
+            bool bit = (currentByte >> i) & 1;
+            if (bit)
+                current = current->right;
+            else
+                current = current->left;
+
+            if (current->left == NULL && current->right == NULL) {
+                fwrite(&current->data, sizeof(unsigned char), 1, decompressedFile);
+                current = root;
             }
         }
     }
-    if (bit_count > 0) {
-        buffer <<= (8 - bit_count);
-        fputc(buffer, output_fp);
-    }
+
+    // Освобождение памяти, закрытие файлов
+    fclose(compressedFile);
+    fclose(decompressedFile);
+    free(root);
 }
 
-
 int main() {
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE);
-    printf("Hello, my user.\nThis program allows you to compress files using the Huffman algorithm."
-        "\nFirst, create a file and write its name to the console.\n");
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_GREEN);
-    char from[50];
-    scanf("%s", from);
-    FILE* input_fp = fopen(from, "rb");
-    FILE* output_fp = fopen("output.bin", "wb");
-    FILE* code_fp = fopen("code.txt", "w");
+    setlocale(LC_ALL,".1251");
 
-    printf("input_fp = %p\n", input_fp);
-    printf("output_fp = %p\n", output_fp);
-    printf("code_fp = %p\n", code_fp);
+    const char* sourceFilePath = "input.txt";
+    const char* compressedFilePath = "compressed.bin";
+    const char* decompressedFilePath = "decompressed.txt";
 
-    if (!input_fp) {
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN | BACKGROUND_RED);
-        fprintf(stderr, "Error while opening files\n");
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_GREEN);
-        return 1;
-    }
-    fseek(input_fp, 0L, SEEK_END);
-    long length = ftell(input_fp);
-    char freq[MAX_TREE_HT];
-    fseek(input_fp, 0, SEEK_SET);
-    for (int i = 0; i < length; ++i) {
-        freq[(unsigned char)fgetc(input_fp)]++;
-    }
-    //    printf("input_fp = %p\n", input_fp);
-    //    printf("output_fp = %p\n", output_fp);
-    //    printf("code_fp = %p\n", code_fp);
+    compressFile(sourceFilePath, compressedFilePath);
+    decompressFile(compressedFilePath, decompressedFilePath);
 
-    encode(input_fp, output_fp, code_fp);
-    fclose(input_fp);
-    fclose(output_fp);
-    fclose(code_fp);
+    printf("Сжатие и распаковка завершены.\n");
+
     return 0;
 }
